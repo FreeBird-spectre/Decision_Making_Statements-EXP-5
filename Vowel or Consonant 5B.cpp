0e2b81be0d61932e8ff8e525b3eb42430c3f43af
FreeBird-spectre <sethabir699@gmail.com>
2025-07-22 19:46:03 +0530

    Update Vowel or Consonant 5B.cpp

--- Vowel or Consonant 5B.cpp
+++ Vowel or Consonant 5B.cpp
@@ -1,1 +1,25 @@
+// Abir Seth
+// PRN- 24070123003
+// Entc A1
 
+#include <iostream>
+using namespace std;
+
+int main() {
+char ch;
+cout << "Enter a character: ";
+cin >> ch;
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        cout << ch << " is a vowel." << endl;
+    } else {
+        cout << ch << " is a consonant." << endl;
+    }
+
+    return 0;
+}
+
+Enter a character: r
+r is a consonant.
+=== Code Execution Successful ===
