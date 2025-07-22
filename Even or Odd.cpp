21e793024b6bf548c8ab7080866794ad03b56339
FreeBird-spectre <sethabir699@gmail.com>
2025-07-22 19:41:46 +0530

    Update Even -or- Odd.cpp

--- Even or Odd.cpp
+++ Even or Odd.cpp
@@ -1,1 +1,27 @@
+// Abir Seth
+// PRN- 24070123003
+// Entc A1
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter an integer: ";
+    cin>>number;
+    if (number%2==0)
+    {
+        cout<<number<<"is even"<<endl;
+    }
+    else
+    {
+        cout<<number<<"is odd"<<endl;
+    }
+    return 0;
+}
+
+Enter an integer: 56
+56is even
+
+
+=== Code Execution Successful ===
